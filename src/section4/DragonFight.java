cc4cd4c23c5671565730f873056a0bc2d80614a4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-09 14:49:53 -0700

    Day 4

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -7,59 +7,76 @@ import javax.swing.ImageIcon;
 public class DragonFight {
 	// 1. Create a main method and put all of the code below inside of it
 	public static void main(String[] args) {
-		
-		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
-		
+
+		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");
+
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
-		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
-		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		// 2. Create a variable called "playerHealth" to store your health (set it equal
+		// to 100)
+		int hp = 100;
+		// 3. Create a variable called "dragonHealth" to store the dragon's health (set
+		// it equal to 100)
+		int dhp = 100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int pd = 1;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
+		int dd = 1;
+
+		// 6. Delete the slashes at the beginning of the next line.
+		while (hp > 0 && dhp > 0) { // this line of code keeps the battle going until someone's health reaches 0
+
+			// 7. Add a closing mustache at the very bottom of this program (since we just
+			// added an opening mustache on the previous step).
+
+			// 8. Ask the player in a pop-up if they want to attack the dragon with a yell
+			// or a kick
+			String action = JOptionPane.showInputDialog(null, "Do you want to yell or kick.");
+			// 9. If they typed in "yell":
+
 		
-		
-		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
-		
-		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
-		
-		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
-		// 9. If they typed in "yell":
-		
-			//-- Find a random number between 0 and 10 and store it in dragonDamage
 			
-			//-- Subtract that number from the dragon's health variable 
-			
-		// 10. If they typed in "kick":
-		
-			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
-			//-- Subtract that number from the dragon's health variable
-			
-			
-		
-		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
-		// 12. Subtract this number from the player's health
-		
-		
-		
-		// 13. If the user's health is less than or equal to 0
-		
-			//-- Tell the user that they lost
-			
-		
-		// 14. Else if the dragon's health is less than or equal to 0
-		
-			//-- Tell the user that the dragon is dead and they took a ton of gold!
-			
-	   //  15.  Else
-			
-			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
-			
+			// -- Find a random number between 0 and 10 and store it in dragonDamage
+		if (action.equalsIgnoreCase("yell")) {
+			dd = new Random().nextInt(11);
+			// -- Subtract that number from the dragon's health variable
+		 dhp = dhp - dd;
+			// 10. If they typed in "kick":
 		}
+			// -- Find a random number between 0 and 25 and store it in dragonDamage
+		else { 
+		dd = new Random().nextInt(26);
+		 dhp = dhp - dd;
+		}
+			// -- Subtract that number from the dragon's health variable
+
+			// 11. Find a random number between 0 and 35 and store it in playerDamage
+			 pd = new Random().nextInt(36);
+
+			// 12. Subtract this number from the player's health
+			 hp = hp - pd;
+
+			// 13. If the user's health is less than or equal to 0
+			if (hp <= 0) { // -- Tell the user that they lost
+				JOptionPane.showMessageDialog(null, "You Died");
+			}
+			// 14. Else if the dragon's health is less than or equal to 0
+			else if (dhp <= 0) {
+				JOptionPane.showMessageDialog(null, "You Won.");
+			}
+			// -- Tell the user that the dragon is dead and they took a ton of gold!
+
+			// 15. Else
+			else {
+				JOptionPane.showMessageDialog(null, "Your health: " + hp + " The dragons health: " + dhp + ".");
+
+				// -- Pop up a message that tells the their current health and the dragon's
+				// currently health (Bonus: Also display the amount of health that was lost for
+				// each player this round)
+
+			}
+
+		}
+
 	}
 
+}
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -3,31 +3,30 @@ package section4;
 import javax.swing.JOptionPane;
 
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
-		
-		// 1.  Create a variable to hold the user's score 
+
+		// 1. Create a variable to hold the user's score
 		int score = 0;
-		// 2.  Ask the user a question 
+		// 2. Ask the user a question
 		String Q1 = JOptionPane.showInputDialog(null, "Are you in the middle classroom of three? Yes or No.");
-		// 3.  Use an if statement to check if their answer is correct
-		if(Q1.equalsIgnoreCase("Yes")) {
-			score ++;
+		// 3. Use an if statement to check if their answer is correct
+		if (Q1.equalsIgnoreCase("Yes")) {
+			score++;
 		}
-		// 4.  if the user's answer is correct
-		String Q2 = JOptionPane.showInputDialog(null, "Some types of eels can use camoflauge. Yes or No." );
-				if(Q2.equalsIgnoreCase("No")) {
-					score ++;
-				}
-		String Q3 = JOptionPane.showInputDialog(null, "Is there a type of sea creature known as a cuddle fish? Yes or No." );
-				if(Q3.equalsIgnoreCase("No")) {
-					score ++;
-	
-	
-					
-				}				
-				
-				JOptionPane.showMessageDialog(null, "Your score was " + score);
-	
+		// 4. if the user's answer is correct
+		String Q2 = JOptionPane.showInputDialog(null, "Some types of eels can use camoflauge. Yes or No.");
+		if (Q2.equalsIgnoreCase("No")) {
+			score++;
+		}
+		String Q3 = JOptionPane.showInputDialog(null,
+				"Is there a type of sea creature known as a cuddle fish? Yes or No.");
+		if (Q3.equalsIgnoreCase("No")) {
+			score++;
+
+		}
+
+		JOptionPane.showMessageDialog(null, "Your score was " + score);
+
 	}
-	}
+}
