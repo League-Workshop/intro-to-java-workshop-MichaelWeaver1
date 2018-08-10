003011dc5dbf99abdc31fb135f2c8d9323728157
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-10 14:51:49 -0700

    Day 4

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -7,19 +7,21 @@ public class Spiral {
 	public static void main(String[] args) {
 		// 1. Create a new Robot
 		Robot marvin = new Robot();
+		marvin.hide();
+		marvin.setPenWidth(200);
 		// 5. Set your robot's pen down
 marvin.penDown();
 		// 3. Set the robot to go at max speed (100)
 		marvin.setSpeed(230);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		for (int i = 0; i < 75; i++) {
+		for (int i = 0; i < 2000; i++) {
 
 			// 7. Change the pen color to random
 marvin.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
 marvin.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-			marvin.turn(360 / 7);
+			marvin.turn(360 / 3);
 			// 8. Set the pen width to i
 		marvin.setPenWidth(i);
 		
--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,44 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int rand = new Random().nextInt(4);
 	// 3. Print out this variable
-
+System.out.println(rand);
 	// 4. Get the user to enter something that they think is awesome
-
+String thing = JOptionPane.showInputDialog("What is something awesome.");
 	// 5. If the random number is 0
-
+if (rand == 0) {
+	JOptionPane.showInternalMessageDialog(null, "That is awesome.");
+}
 	// -- tell the user whatever they entered is awesome!
 
 	// 6. If the random number is 1
-
+else if (rand == 1) {
+	JOptionPane.showInternalMessageDialog(null, "Hmmmm, thats okay.");
+}
 	// -- tell the user whatever they entered is ok.
 
 	// 7. If the random number is 2
-
+else if (rand == 2) {
+	JOptionPane.showInternalMessageDialog(null, "That's boring.");
+}
 	// -- tell the user whatever they entered is boring.
 
 	// 8. If the random number is 3
-
+else if (rand == 3) {
+	JOptionPane.showInternalMessageDialog(null, "That is incre-di-bles.");
+}
 	// -- write your own answer
-
+}
 }
 
 
